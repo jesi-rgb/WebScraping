4d81e98a33206f6597555f37608c3ac0a1b82944
Jesús Enrique Cartas Rascón <blograso@gmail.com>
2019-11-05 16:44:23 +0100

    getting useful info, just need to store it now

--- webs.py
+++ webs.py
@@ -6,9 +6,11 @@ driver = webdriver.Chrome("/Users/jesi/anaconda3/chromedriver")
 
 marcas = ['Renault/', 'Seat/', 'Toyota/', 'Volkswagen/', 'Ford/']
 
-cars = []  #List to store name of the product
-prices = []  #List to store price of the product
-links = []  #List to store rating of the product
+cars = []
+prices = []
+links = []
+
+full_links = []
 
 for m in marcas:
     driver.get("https://www.coches.com/coches-nuevos/"+m)
@@ -16,13 +18,59 @@ for m in marcas:
 
     soup = BeautifulSoup(content, features="lxml")
 
-    for r in soup.findAll(attrs={'class':'cc_new_card_result'}):
-        name = r.find('span', attrs={'class':'cc_make_model'}).text
-        price = r.find('span', attrs={'class':'cc_new_price'}).text
-        link = r.find('a', attrs={'class':'cc_icon_link'}).href
+    for r in soup.find_all(attrs={'class':'cc_new_card_result'}):
+        # name = r.find('span', attrs={'class':'cc_make_model'}).text
+        # price = r.find('span', attrs={'class':'cc_new_price'}).text
+        link = r.find('a', attrs={'class':'cc_icon_link'})['href']
 
-        cars.append(name)
-        prices.append(price)
+        # cars.append(name)
+        # prices.append(price)
         links.append(link)
 
-links
+
+for l in links:
+    driver = webdriver.Chrome("/Users/jesi/anaconda3/chromedriver")
+    driver.get(l)
+    content = driver.page_source
+    soup = BeautifulSoup(content, features="lxml")
+
+    price_list = soup.find_all('div', attrs={'class':'cc_prices_list'})
+
+    for p in price_list:
+        # full_name = p.find_all('td', attrs={'class':'cc_col col_1'})
+        # horse_power = p.find_all('td', attrs={'class':'cc_col col_2'})
+        link_list = p.find_all('td', attrs={'class':'cc_col col_6'})
+        for e in link_list:
+            link = e.find('a')['href']
+            full_links.append(link)
+
+    driver.close()
+
+
+for l in full_links:
+    driver = webdriver.Chrome("/Users/jesi/anaconda3/chromedriver")
+    driver.get(l)
+    content = driver.page_source
+    soup = BeautifulSoup(content, features="lxml")
+
+    nombre = soup.find('span', attrs={'class':'cc_principal'}).text
+    precio = soup.find('div', attrs={'class':'cc_coches_price'}).text
+
+    cars.append(nombre)
+    prices.append(precio)
+
+    div_prestaciones = soup.find('div', attrs={'class':'cc_performance'})
+
+    lista_prestaciones = div_prestaciones.find_all('span', attrs={'class':'cc_data'})
+
+    for p in lista_prestaciones:
+        categoria = p.find_all('span', attrs={'class':'cc_label'})
+        valor = p.find_all('strong')
+        for elem in categoria:
+            print(elem.text)
+        for elem in valor:
+            print(elem.text)
+
+
+
+    driver.close()
