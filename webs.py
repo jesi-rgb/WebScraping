42aeabd03ffec2098dce45a4b1faee8c22b31479
Jesús Enrique Cartas Rascón <blograso@gmail.com>
2019-11-06 10:04:45 +0100

    saving to csv

--- webs.py
+++ webs.py
@@ -17,54 +17,69 @@ cars = []
 prices = []
 links = []
 full_links = []
+prestaciones = dict()
 
-for m in marcas:
-    driver.get("https://www.coches.com/coches-nuevos/" + m)
-    content = driver.page_source
 
-    soup = BeautifulSoup(content, features="lxml")
+def extract():
+    for m in marcas:
+        driver.get("https://www.coches.com/coches-nuevos/" + m)
+        content = driver.page_source
 
-    for r in soup.find_all(attrs={'class': 'cc_new_card_result'}):
-        link = r.find('a', attrs={'class': 'cc_icon_link'})['href']
-        links.append(link)
+        soup = BeautifulSoup(content, features="lxml")
 
-for l in links:
-    driver = webdriver.Chrome("/Users/jesi/anaconda3/chromedriver",
-                              options=chrome_options)
-    driver.get(l)
-    content = driver.page_source
-    soup = BeautifulSoup(content, features="lxml")
+        for r in soup.find_all(attrs={'class': 'cc_new_card_result'}):
+            link = r.find('a', attrs={'class': 'cc_icon_link'})['href']
+            links.append(link)
 
-    price_list = soup.find_all('div', attrs={'class': 'cc_prices_list'})
+    print("#######Marcas cogidas##########")
+    for l in links:
+        # driver = webdriver.Chrome("/Users/jesi/anaconda3/chromedriver",
+        # options=chrome_options)
+        driver.get(l)
+        content = driver.page_source
+        soup = BeautifulSoup(content, features="lxml")
 
-    for p in price_list:
-        link_list = p.find_all('td', attrs={'class': 'cc_col col_6'})
-        for e in link_list:
-            link = e.find('a')['href']
-            full_links.append(link)
+        price_list = soup.find_all('div', attrs={'class': 'cc_prices_list'})
 
-for l in full_links:
-    driver = webdriver.Chrome("/Users/jesi/anaconda3/chromedriver",
-                              options=chrome_options)
-    driver.get(l)
-    content = driver.page_source
-    soup = BeautifulSoup(content, features="lxml")
+        for p in price_list:
+            link_list = p.find_all('td', attrs={'class': 'cc_col col_6'})
+            for e in link_list:
+                link = e.find('a')['href']
+                full_links.append(link)
 
-    nombre = soup.find('span', attrs={'class': 'cc_principal'}).text
-    precio = soup.find('div', attrs={'class': 'cc_coches_price'}).text
+    print("#######Modelos cogidos##########")
+    for l in full_links:
+        # driver = webdriver.Chrome("/Users/jesi/anaconda3/chromedriver",
+        # options=chrome_options)
+        driver.get(l)
+        content = driver.page_source
+        soup = BeautifulSoup(content, features="lxml")
 
-    cars.append(nombre)
-    prices.append(precio)
+        nombre = soup.find('span', attrs={'class': 'cc_principal'}).text
+        precio = soup.find('div', attrs={'class': 'cc_coches_price'}).text
 
-    div_prestaciones = soup.find('div', attrs={'class': 'cc_performance'})
+        cars.append(nombre)
+        prices.append(precio)
 
-    lista_prestaciones = div_prestaciones.find_all('span',
-                                                   attrs={'class': 'cc_data'})
+        div_prestaciones = soup.find('div', attrs={'class': 'cc_performance'})
 
-    for p in lista_prestaciones:
-        categoria = p.find_all('span', attrs={'class': 'cc_label'})
-        valor = p.find_all('strong')
-        for elem in categoria:
-            print(elem.text)
-        for elem in valor:
-            print(elem.text)
+        lista_prestaciones = div_prestaciones.find_all(
+            'span', attrs={'class': 'cc_data'})
+
+        for p in lista_prestaciones:
+            categoria = p.find_all('span', attrs={'class': 'cc_label'})
+            valor = p.find_all('strong')
+
+            for c, valor in zip(categoria, valor):
+                prestaciones[c] = v
+
+    datos = pd.DataFrame({
+        'Car': products,
+        'Price': prices,
+        'Specs': prestaciones
+    })
+    datos.to_csv('products.csv', index=False, encoding='utf-8')
+
+
+if __name__ == '__main__':
+    extract()
